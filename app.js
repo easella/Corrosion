ad363bb7b16fb4126ccac6652c8c9a1372f1ebd7
UnnamedNoob <81578319+UnnamedNoob@users.noreply.github.com>
2021-08-11 22:06:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ proxy.bundleScripts();
 
 server.on('request', (request, response) => {
     if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
-    response.end(fs.readFileSync('index.html', 'utf-8'));
+    response.end(fs.readFileSync('demo/index.html', 'utf-8'));
 }).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(process.env.PORT);
 
 console.log("https server listening on "+process.env.PORT)
