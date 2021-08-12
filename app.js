6c79cd976f334e5a4008c6f3dc2887078716607d
UnnamedNoob <81578319+UnnamedNoob@users.noreply.github.com>
2021-08-12 10:01:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,102 +1,20 @@
-/* -----------------------------------------------
- * Authors: QuiteAFancyEmerald, BinBashBanana (OlyB), YÖCTDÖNALD'S
- * Additional help from Divide and SexyDuceDuce
- * MIT license: http://opensource.org/licenses/MIT
- * ----------------------------------------------- */
-const
-    corrosion = require('corrosion'),
-    path = require('path'),
-    fs = require('fs'),
-    http = require('http'),
-    express = require('express'),
-    app = express(),
-    port = process.env.PORT || config.port,
-    server = http.createServer(app);
-
-btoa = (str) => {
-    return new Buffer.from(str).toString('base64');
-}
-
-atob = (str) => {
-    return new Buffer.from(str, 'base64').toString('utf-8');
-}
-
-const text404 = fs.readFileSync(path.normalize(__dirname + '/demo/index.html'), 'utf8'),
-    siteIndex = 'demo/index.html',
-    cookingInserts = [
-        "Random sentence one filled with certain keywords.",
-        "Random sentence two filled with certain keywords."
-    ],
-    splashtext = [
-        'Th<wbr>is is si<wbr>mp<wbr>ly a pu<wbr>bl<wbr>ic dem<wbr>o for Ho<wbr>ly Unb<wbr>locke<wbr>r. Certain functions may not work properly. Jo<wbr>in the <a id="tnlink" target="_blank">T<wbr>N Disc<wbr>ord</a> for official site lin<wbr>ks'
-    ],
-    vegetables = ['Cooking1', 'Cooking2'],
-    charRandom = ['&#173;', '&#8203;'];
-
-function randomListItem(lis) {
-    return lis[Math.floor(Math.random() * lis.length)];
-}
-
-// The inline function returns are necessary to prevent all replaced things from being the same
-
-function insertCharset(str) {
-    return str.replace(/&#173;|&#8203;|<wbr>/g, function() { return randomListItem(charRandom); });
-}
-
-function insertSplash(str) {
-    return str.replace(/&#xFE0F;/g, function() { return splashtext; });
-}
-
-function insertCooking(str) {
-    return str.replace(/<!-- IMPORTANT-HUCOOKINGINSERT-DONOTDELETE -->/g, function() { return '<span style="display: none;" data-fact="' + randomListItem(vegetables) + '" data-type="' + randomListItem(vegetables) + '">' + randomListItem(cookingInserts) + '</span>'; });
-}
-
-function cacheBusting(str) {
-    return str.replace(/styles.min.css/g, 'styles-368357.min.css');
-}
-
-function insertAll(str) {
-    return insertCharset(insertCooking(insertSplash(cacheBusting(str))));
-}
-
-/* Cache Busting
-const stats = fs.statSync(path.normalize(__dirname + fileMod), 'utf8'), 
-	seconds = (new Date().getTime() - stats.mtime);
-fs.renameSync(path.normalize(__dirname + fileMod), 'links-' + seconds + '.js');
-function cacheBusting(str) {
-	return str.replace(/links.js/g, "links" + '-' + seconds + '.js');
-}
-*/
-
-function tryReadFile(file) {
-    return fs.existsSync(file) ? fs.readFileSync(file, 'utf8') : text404;
-}
-
-// Local Corrosion Proxy
-
-const proxy = new corrosion({
-    prefix: '/search/',
-    title: 'Untitled Document',
-    ws: true,
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const ssl = {
+    key: fs.readFileSync(path.join(__dirname, 'demo/ssl.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'demo/ssl.cert')),
+};
+const server = https.createServer(ssl);
+const Corrosion = require('corrosion');
+const proxy = new Corrosion({
     codec: 'xor',
-    requestMiddleware: [
-        corrosion.middleware.blacklist([
-            'accounts.google.com',
-        ], 'Page is blocked'),
-    ],
 });
 
 proxy.bundleScripts();
 
-/* Querystring Navigation */
-app.get('/', async(req, res) => res.send(insertAll(tryReadFile(path.normalize('demo/index.html' + (['/', '/?'].includes(req.url) ? siteIndex : pages[Object.keys(req.query)[0]]))))));
-
-/* Static Files Served */
-app.use(express.static(path.normalize('demo/index.html')));
-app.use((req, res) => {
-    if (req.url.startsWith(proxy.prefix)) return proxy.request(req, res);
-    res.status(404, res.send(insertAll(text404)))
-});
-
-server.listen(port);
-console.log('Holy Unblocker is listening on port ' + port + '. This is simply a public for Holy Unblocker. Certain functions may not work properly.');
+server.on('request', (request, response) => {
+    if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
+    response.end(fs.readFileSync(__dirname + 'demo/index.html', 'utf-8'));
+}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(process.env.PORT);
+console.log("listening on "+process.env.PORT)
