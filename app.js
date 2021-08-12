4de85c91ffe4ec3dae9915139ad636f68e244ca1
UnnamedNoob <81578319+UnnamedNoob@users.noreply.github.com>
2021-08-12 12:22:11 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const Corrosion = require('corrosion');
-const proxy = new Corrosion();
-const http = require('http')
-http.createServer((req, res) => 
-  proxy.request(req, res) // Request Proxy
-).on('upgrade', (req, socket, head) => 
-  proxy.upgrade(req, socket, head) // WebSocket Proxy
-).listen(process.env.PORT);
-console.log("listening on "+process.env.PORT)
